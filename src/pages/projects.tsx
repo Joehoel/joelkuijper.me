3fd4bfdc4b98a8ed943e9c3b2aaaa387fb072d46
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-07-20 17:32:02 +0200

    forgot something

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,7 +5,7 @@ import { projects } from "../../data/projects";
 export default function Projects() {
   return (
     <Container title="Projects - Joël Kuijper" description="Current and previously done projects">
-      <section className="md:masonry before:box-inherit after:box-inherit space-y-6 md:space-y-0">
+      <section className="md:masonry before:box-inherit after:box-inherit space-y-6 md:space-y-0 max-w-7xl mx-auto">
         {projects.map((project, index) => (
           <Project project={project} key={`project-${index}`} />
         ))}
