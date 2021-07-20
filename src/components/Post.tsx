238faaf54789bd9714c3ad249bae66125ca1b434
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-07-20 17:30:05 +0200

    Update blog page

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -9,8 +9,8 @@ interface Props extends Frontmatter {
 const Post = ({ title, description, slug }: Props) => {
   return (
     <Link href={`/blog/${slug}`}>
-      <a className="w-full">
-        <div className="mb-8 w-full">
+      <a className="rounded-md w-full dark:bg-gray-900 border dark:border-gray-800 p-4 shadow-sm hover:scale-[1.01] hover:shadow-md transition-transform">
+        <div className="w-full">
           <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">{title}</h4>
           <p className="text-gray-600 dark:text-gray-400">{description}</p>
         </div>
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -32,7 +32,7 @@ export default function Blog({ posts }: Props) {
             aria-label="Search blog posts"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder="Search blog posts"
-            className="px-4 py-2 focus:outline-none border border-gray-300 dark:border-gray-900 focus:ring-orange-500 focus:border-orange-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+            className="px-4 py-2 focus:outline-none shadow-sm focus:shadow-md focus:scale-[1.01] transition-transform border border-gray-300 dark:border-gray-900 focus:ring-orange-500 focus:border-orange-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
           />
           <svg
             className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
@@ -50,11 +50,13 @@ export default function Blog({ posts }: Props) {
           </svg>
         </div>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
-          All Posts
+          Latest Posts
         </h3>
-        {blogPosts.map((post) => (
-          <Post key={post.slug} {...post} />
-        ))}
+        <div className="flex flex-col gap-4">
+          {blogPosts.map((post) => (
+            <Post key={post.slug} {...post} />
+          ))}
+        </div>
       </section>
     </Container>
   );
